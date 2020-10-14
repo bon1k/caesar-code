d594f0665fefa9dc6ed929193c2608f9ae6f400a
“Mikhail <bon1k@bk.ru>
2020-10-14 23:29:27 +0400

    step problem

--- src/CaesarCode.java
+++ src/CaesarCode.java
@@ -16,32 +16,20 @@ public class CaesarCode {
 
 
         List<Character> alphavit = new ArrayList<Character>(Arrays.asList('а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к',
-                    'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'));
+                'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'));
 
         int step = 1;
         String rez = "";
 
-            for (int j = 0; j < alphavit.size() ; j++) {
-                int repIndex = mail.indexOf(alphavit.get(0));
-                rez = mail.replace(mail.charAt(repIndex), alphavit.get(step));
-                ++ step;
-                if (step == 32){
-                 step = step-31;
-                }
-                System.out.println(rez);
+        for (int j = 0; j < alphavit.size() ; j++) {
+            int repIndex = mail.indexOf(alphavit.get(0));
+            rez = mail.replace(mail.charAt(repIndex), alphavit.get(step));
+            ++ step;
+            if (step == 32){
+                step = step-31;
             }
-
-
-//
-//        if (mail.charAt(0) == alphavit.get(5)){
-//           String rez =  mail.replace(mail.charAt(0), alphavit.get(5+3));
-//            System.out.println(rez);
-//        }
-
-
-
-
-
+            System.out.println(rez);
+        }
 
     }
 }
