fc8470e946d60cf1fbe7100ffc92cbaac2a33e7b
Mikhail Gorshkov <bon1k@bk.ru>
2020-10-15 15:54:50 +0400

    code 11

--- src/CaesarCode.java
+++ src/CaesarCode.java
@@ -10,26 +10,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CaesarCode {
-    public static void main(String[] args) {
-        String mail = ("Еъёчхф Вхзёюлх, адздёиу ф ждэщхб")
-                .replaceAll("\\p{Punct}"," ").toLowerCase();;
-
+    static int repIndex;
+    public static void main(String[] args)  {
+        String mail = ("Еъёчхф Вхзёюлх")
+                .replaceAll("\\p{Punct}"," ").toLowerCase();
 
         List<Character> alphavit = new ArrayList<Character>(Arrays.asList('а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к',
+                'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к',
                 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'));
 
+
+        List<Character> rez = new ArrayList<Character>();
         int step = 1;
-        String rez = "";
+        while (step<32){
 
-        for (int j = 0; j < alphavit.size() ; j++) {
-            int repIndex = mail.indexOf(alphavit.get(j));
-            rez = mail.replace(mail.charAt(repIndex), alphavit.get(step));
-            ++ step;
-            if (step == 32){
-                step = step-31;
+        for (int i = 0; i < mail.length(); i++) {
+            for (int j = 0; j < 32; j++) {
+                if (mail.charAt(i) == alphavit.get(j)){
+                   rez.add(alphavit.get(j+step));
+                }
             }
-            System.out.println(rez);
         }
-
+        System.out.println(rez+" "+step);
+        step++;
+        rez.clear();
+        }
     }
 }
+
